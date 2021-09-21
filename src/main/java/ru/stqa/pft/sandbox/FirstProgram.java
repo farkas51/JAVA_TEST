b1b79b5fd20e126a35357a7d11271adbac9b31ae
Groshev Nikolay <kolia.avatar@gmail.com>
2021-09-21 22:44:03 +0300

    ООП подъехало

--- src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/FirstProgram.java
@@ -2,6 +2,16 @@ package ru.stqa.pft.sandbox;
 
 public class FirstProgram {
     public static void main(String[] args){
-        System.out.println("Hello, world!");
+        String s = "User";
+        hello(s);
+        Sqare sa = new Sqare(5);
+        sa.l = 5.0;
+        System.out.println(area(sa));
+    }
+    public static void hello(String s){
+        System.out.println("wwowow, "+ s);
+    }
+    public static double area(Sqare s){
+        return s.l*s.l;
     }
 }
---
+++ src/main/java/ru/stqa/pft/sandbox/Sqare.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class Sqare {
+    public double l;
+    public Sqare(double len){
+        this.l = len;
+    }
+    public  double area(){
+        return this.l*this.l;
+    }
+}
